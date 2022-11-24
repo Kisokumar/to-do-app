5a6b19d9659d80ee1ac7eb0b45c829f5a519f536
Kisho Mahadevan <kisokumar@fb.com>
2022-11-24 10:20:00 +0000

    fix: compuslory arguments for tasks

--- backend/src/models/Task.js
+++ backend/src/models/Task.js
@@ -1,6 +1,5 @@
 const { Sequelize } = require("sequelize");
 const { db, DataTypes } = require("../db/db");
-const uniqueIdGenerator = require("../middleware/uniqueIdGenerator");
 
 const Task = db.define("tasks", {
   id: {
@@ -8,9 +7,10 @@ const Task = db.define("tasks", {
     primaryKey: true,
     defaultValue: Sequelize.UUIDV4,
   },
-  task: { type: DataTypes.STRING, allowNull: false },
+  taskname: { type: DataTypes.STRING, allowNull: false },
   datedue: { type: DataTypes.STRING, allowNull: false },
   priority: { type: DataTypes.STRING, allowNull: true },
+  category: { type: DataTypes.STRING, allowNull: true },
   userId: { type: Sequelize.UUID, allowNull: false },
 });
 
