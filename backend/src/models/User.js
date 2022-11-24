f9e612d59f5986ef52e0bbef6791affd4b6350e7
Kisho Mahadevan <kisokumar@fb.com>
2022-11-24 10:18:21 +0000

    fix: `username` and `email` not compulsory

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -8,10 +8,10 @@ const User = db.define("users", {
     primaryKey: true,
     defaultValue: Sequelize.UUIDV4,
   },
-  fullname: { type: DataTypes.STRING, allowNull: false },
+  fullname: { type: DataTypes.STRING, allowNull: true },
   username: { type: DataTypes.STRING, allowNull: false, unique: true },
   password: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: true },
 });
 
 module.exports = { User };
