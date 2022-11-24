047cd3c277173981b9eb36ecb27295c551d6da8b
Lorena Gil <lorenagil@fb.com>
2022-11-24 18:13:59 +0100

    Added globalusername in login.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,49 @@
-import { useState } from 'react';
-import Register from './components/Register';
-import LogIn from './components/LogIn';
-import './App.css';
+import { useState } from "react";
+import  Register from "./components/Register";
+import { LogIn, globalusername } from "./components/LogIn";
+import "./App.css";
 
 function App() {
   const [isRegisterShown, setIsRegisterShown] = useState(false);
   const [isLogInShown, setIsLogInShown] = useState(false);
 
-  function registerClick (event){
+  function registerClick(event) {
     if (isLogInShown) {
-      setIsLogInShown(current => !current);
+      setIsLogInShown((current) => !current);
     }
-    setIsRegisterShown(current => !current);
-
+    setIsRegisterShown((current) => !current);
   }
 
-  function logInClick (event){
+  function logInClick(event) {
     if (isRegisterShown) {
-      setIsRegisterShown(current => !current);
+      setIsRegisterShown((current) => !current);
     }
-    setIsLogInShown(current => !current);
+    setIsLogInShown((current) => !current);
   }
 
   return (
     <div className="App">
       <header className="App-header">
-      <div id="title">
-        <div id="title-to">to-</div>
-        <div id="title-do">do</div>
-      </div>
+        <div id="title">
+          <div id="title-to">to-</div>
+          <div id="title-do">do</div>
+        </div>
 
-      <div id="header-buttons">
-        <button className="header-btn" id="login" onClick={logInClick}>sign in</button>
-        <button className="header-btn" id="register" onClick={registerClick}>register</button>
-      </div>
-
+        <div id="header-buttons">
+          <button className="header-btn" id="login" onClick={logInClick}>
+            sign in
+          </button>
+          <button className="header-btn" id="register" onClick={registerClick}>
+            register
+          </button>
+        </div>
       </header>
       <main>
-        {isRegisterShown && <Register setIsRegisterShown={setIsRegisterShown}/>}
-        {isLogInShown && <LogIn setIsLogInShown={setIsLogInShown}/>}
+        {isRegisterShown && (
+          <Register setIsRegisterShown={setIsRegisterShown} />
+        )}
+        {isLogInShown && <LogIn setIsLogInShown={setIsLogInShown} />}
+        {console.log("login", globalusername)}
       </main>
     </div>
   );
