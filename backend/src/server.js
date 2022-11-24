bfb936a78d901fc0a957114227df74a50c36939a
Lorena Gil <lorenagil@fb.com>
2022-11-24 17:01:49 +0100

    Added cors and fixed fetchData function.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { db } = require("./db/db");
+const cors = require("cors");
 const app = express();
 
 const userRouter = require("./routes/User");
 
+app.use(cors);
 app.use(express.json());
 
 app.use("/users", userRouter);
