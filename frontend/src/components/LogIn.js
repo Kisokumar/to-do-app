9b809afe1e479f104bca807f901b1188be5b859b
Kisho Mahadevan <kisokumar@fb.com>
2022-11-25 16:35:25 +0000

    styling: colours and borders

--- frontend/src/components/LogIn.js
+++ frontend/src/components/LogIn.js
@@ -1,48 +1,54 @@
-import { useState } from "react"
+import { useState } from "react";
 import userExists from "../utils/userExists";
-
-
 
 let globalusername;
 
 function LogIn(props) {
-
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
 
   function submitLogIn(event) {
-    event.preventDefault()
+    event.preventDefault();
     const user = {
       username: username,
-      password: password
-    }
+      password: password,
+    };
 
     console.log(user);
     if (userExists) {
       console.log("Log in completed successfully");
-      props.setIsUserLogged(current => !current);
-    }
-    else {
+      props.setIsUserLogged((current) => !current);
+    } else {
       alert("Incorrect username or password");
     }
-    props.setIsLogInShown(current => !current);
+    props.setIsLogInShown((current) => !current);
   }
 
   globalusername = username;
 
-  return(
+  return (
     <div className="form-space">
       <div></div>
       <form className="user-form" onSubmit={submitLogIn}>
-        <label>username: </label><br></br>
-        <input onChange={(e) => setUsername(e.target.value)} placeholder="Username" /><br></br>
-        <label>password: </label><br></br>
-        <input onChange={(e) => setPassword(e.target.value)} placeholder="Password" /><br></br>
-        <button className="form-btn" type="submit">Log in</button>
+        <p className="form-title-text">Sign in:</p>
+        <input
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+          className="input-box"
+        />
+        <br></br>
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          className="input-box"
+        />
+        <br></br>
+        <button className="form-btn" type="submit">
+          Log in
+        </button>
       </form>
     </div>
-
-  )
+  );
 }
 
-export {LogIn, globalusername}
+export { LogIn, globalusername };
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState } from "react";
 import fetchData from "../utils/fetchData";
 import registerUser from "../utils/resgisterUser";
 
@@ -10,17 +10,17 @@ export default function Register(props) {
   const [password, setPassword] = useState();
 
   function submitRegister(event) {
-    event.preventDefault()
+    event.preventDefault();
     const newUser = {
       fullname: name + " " + surname,
       username: username,
       password: password,
-      email:email
-    }
+      email: email,
+    };
 
     try {
       registerUser(newUser);
-      props.setIsRegisterShown(current => !current);
+      props.setIsRegisterShown((current) => !current);
       console.log("Registration completed successfully");
       console.log("LogIn to start using to-do");
     } catch (error) {
@@ -28,22 +28,49 @@ export default function Register(props) {
     }
   }
 
-  return(
+  return (
     <div className="form-space">
       <div></div>
-      <form className="user-form" onSubmit={submitRegister}>
-        <label>name: </label><br></br>
-        <input onChange={(e) => setName(e.target.value)} placeholder="Name" /><br></br>
-        <label>surname: </label><br></br>
-        <input onChange={(e) => setSurname(e.target.value)} placeholder="Surname" /><br></br>
-        <label>email: </label><br></br>
-        <input onChange={(e) => setEmail(e.target.value)} placeholder="Email" /><br></br>
-        <label>username: </label><br></br>
-        <input onChange={(e) => setUsername(e.target.value)} placeholder="Username" /><br></br>
-        <label>password: </label><br></br>
-        <input onChange={(e) => setPassword(e.target.value)} placeholder="Password" /><br></br>
-        <button className="form-btn" type="submit">Register</button>
+      <form
+        className="input-box"
+        className="user-form"
+        onSubmit={submitRegister}
+      >
+        <p className="form-title-text">Register:</p>
+        <input
+          className="input-box"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Name"
+        />
+        <br></br>
+        <input
+          onChange={(e) => setSurname(e.target.value)}
+          placeholder="Surname"
+          className="input-box"
+        />
+        <br></br>
+        <input
+          className="input-box"
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <br></br>
+        <input
+          onChange={(e) => setUsername(e.target.value)}
+          className="input-box"
+          placeholder="Username"
+        />
+        <br></br>
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          className="input-box"
+        />
+        <br></br>
+        <button className="form-btn" type="submit">
+          Register
+        </button>
       </form>
     </div>
-  )
+  );
 }
