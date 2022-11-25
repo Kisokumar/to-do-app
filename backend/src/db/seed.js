d0b2b95a3f6fbe2c27cc0615e9c8454a23a9098e
Kisokumar Mahadevan <97011836+Kisokumar@users.noreply.github.com>
2022-11-25 10:16:46 +0000

    run/install scripts (#15)

--- backend/src/db/seed.js
+++ backend/src/db/seed.js
@@ -6,22 +6,25 @@ const path = require("path");
 const seed = async () => {
   await db.sync({ force: true });
   console.log("Database created and reset!");
-
-  mockData = true; //this option should option should be false when deployed/ project is finished
-  if (mockData === true) {
-    const taskSeedPath = path.join(__dirname, "/mocks/task.json");
-    const userSeedPath = path.join(__dirname, "/mocks/user.json");
-    const buffer = await fs.readFile(taskSeedPath);
-    const userBuffer = await fs.readFile(userSeedPath);
-    const { tasksData } = JSON.parse(String(buffer));
-    const { usersData } = JSON.parse(String(userBuffer));
-    const TaskPromises = tasksData.map((task) => Task.create(task));
-    const UserPromises = usersData.map((user) => User.create(user));
-    await Promise.all(TaskPromises);
-    await Promise.all(UserPromises);
-    console.log("Mock data has been seeded!");
-  }
 };
+
+const mock = async () => {
+  await db.sync({ force: true });
+  const taskSeedPath = path.join(__dirname, "/mocks/task.json");
+  const userSeedPath = path.join(__dirname, "/mocks/user.json");
+  const buffer = await fs.readFile(taskSeedPath);
+  const userBuffer = await fs.readFile(userSeedPath);
+  const { tasksData } = JSON.parse(String(buffer));
+  const { usersData } = JSON.parse(String(userBuffer));
+  const TaskPromises = tasksData.map((task) => Task.create(task));
+  const UserPromises = usersData.map((user) => User.create(user));
+  await Promise.all(TaskPromises);
+  await Promise.all(UserPromises);
+  console.log("Mock data has been seeded!");
+};
+
+// comment this line out for no mock data
+mock();
 
 seed();
 
